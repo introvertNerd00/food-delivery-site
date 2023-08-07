abb8475573acb6b2fcca557013f3feae55625104
Hamza <hamza.afzalkhan9@gmail.com>
2023-08-07 13:23:23 +0500

    fixed typos

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,12 +3,12 @@ import { categories } from '../data/data'
 const Category = () => {
   return (
     <div className='max-w-[1640px] mx-auto px-4 py-12'>
-        <h1 className='text-orange-600 before:font-bold text-center text-4xl'>Top Rated Items</h1>
+        <h1 className='text-orange-600 font-bold text-center text-4xl'>Top Rated Items</h1>
         {/*Categories*/}
 
         <div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-4'>
             {categories.map((item, index) =>
-                 <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center '>
+                 <div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center hover:scale-110'>
                     <h2 className='font-bold sm:text-xl'>{item.name}</h2>
                     <img src={item.image} alt={item.name} className=' w-20' />
 
