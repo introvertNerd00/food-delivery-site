d7fe8dd7c98524cb2dd97ddcc47eae770dfa1e72
Hamza <hamza.afzalkhan9@gmail.com>
2023-08-07 12:59:09 +0500

    added food cards

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React,{useState} from 'react'
+import{data} from '../data/data.js'
 
 const Food = () => {
+  
+  const [foods,showFoods]=useState(data);
+  const filterType=(category)=>{
+    showFoods(
+      data.filter((item)=>{
+        return item.category===category}) )
+  }
+
+  const filterPrice=(price)=>{
+    showFoods(
+      data.filter((item)=>{
+        return item.price===price}) )
+  }
+  
   return (
-    <div>Food</div>
+    //console.log(data),
+
+
+    <div className='max-w-[1640] m-auto px-4 py-12  '>
+      <h1 className='text-orange-600 font-bold text-4xl text-center '>Top Rated </h1>
+      {/*Filter Rows*/}
+      <div className='flex flex-col lg:flex-row justify-between '>
+        {/*Filter type*/}
+        <div>
+          <p className='font-bold text-gray-700'>Filter Type</p>
+          <div className='flex justify-between flex-wrap'>
+            <button onClick={()=>showFoods(data)} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>All</button>
+            <button onClick={()=>filterType('burger')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Burgers</button>
+            <button onClick={()=>filterType('pizza')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Pizza</button>
+            <button onClick={()=>filterType('salad')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Salad</button>
+            <button onClick={()=>filterType('chicken')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>Chicken</button>
+          </div>
+        </div>
+
+        {/*Filter price*/}
+        <div>
+          <p className='font-bold text-gray-700'>Filter Price</p>
+          <div className='flex justify-between max-w-[390px] w-full'>
+            <button onClick={()=>filterPrice('$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$</button>
+            <button onClick={()=>filterPrice('$$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$</button>
+            <button onClick={()=>filterPrice('$$$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$$</button>
+            <button onClick={()=>filterPrice('$$$$')} className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1'>$$$$</button>
+
+          </div>
+        </div>
+      </div>
+
+
+      {/*Food Cards*/}
+
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4 '>
+      {foods.map((item,index)=>
+      <div key={index} className='border shadow-lg hover:scale-105 duration-300 rounded-lg  '>
+        <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg'/>
+       <div className='flex justify-between px-2 py-4'>
+       <p className='font-bold'>{item.name}</p>
+        <p><span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span> </p>
+       </div>
+      </div>
+      )}
+
+      </div>
+    </div>
   )
 }
 
